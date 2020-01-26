c02d5dcdc66ac9bd40e5d31600aa7c3f76739885
MaksymY <maksym.yankivskyypro@gmail.com>
2020-01-26 20:57:04 +0100

    First commit

---
+++ questions.js
@@ -0,0 +1,52 @@
+const questionsTemplate = [
+  {
+    question: "en quelle année a était crée le Javascript?",
+    answers: [
+      { letter: "a", answer: "2000" },
+      { letter: "b", answer: "1995" },
+      { letter: "c", answer: "2003" },
+      { letter: "d", answer: "1993" },
+    ],
+    correct: "b",
+  },
+  {
+    question: "quelle est le symbole de l'Ukraine?",
+    answers: [
+      { letter: "a", answer: "Le Kourka" },
+      { letter: "b", answer: "Le Voron" },
+      { letter: "c", answer: "La Lesia" },
+      { letter: "d", answer: "Le Tryzou" },
+    ],
+    correct: "d",
+  },
+  {
+    question: "a partir du Javascript peut ont styliser les élément du DOM?",
+    answers: [
+      { letter: "a", answer: "Oui" },
+      { letter: "b", answer: "Non" },
+      { letter: "c", answer: "Seulements les élements possédent une ID" },
+      { letter: "d", answer: "ni oui ni non" },
+    ],
+    correct: "a",
+  },
+  {
+    question: "quelle est la meileurs série actuellment sur Amazon Prime?",
+    answers: [
+      { letter: "a", answer: "The Boys" },
+      { letter: "b", answer: "Batman For Ever" },
+      { letter: "c", answer: "Star Trek Picard" },
+      { letter: "d", answer: "American Gods" },
+    ],
+    correct: "a",
+  },
+  {
+    question: "en quelle année a commencé la Seconde Guerre Mondial?",
+    answers: [
+      { letter: "a", answer: "1er septembre 1939" },
+      { letter: "b", answer: "22 juin 1941" },
+      { letter: "c", answer: "5 novembre 1940" },
+      { letter: "d", answer: "Y avait pas de 2em Guerre Mondial" },
+    ],
+    correct: "a",
+  },
+];
---
+++ quizz.js
@@ -0,0 +1,120 @@
+let user, questions, answered, selected;
+
+console.log(questionsTemplate);
+
+const startGame = () => {
+  const menu = document.querySelector(".menu");
+  const input = document.querySelector(".input[name=user]");
+  const error = document.querySelector(".error");
+
+  if (input.value.length > 0) {
+    user = input.value;
+    menu.style.display = "none";
+    renderTemplate();
+  } else error.textContent = "Veillez entrer un nom";
+};
+
+const renderTemplate = () => {
+  const quizGame = document.querySelector(".game");
+  quizGame.innerHTML = `
+      <p class="count"></p>
+      <p class="errorQuestion"></p>
+      <p class="question"></p>
+      <section class="content"></section>
+      <button onclick="submitAnswer()" class="next">Suivant</button>
+`;
+  renderQuestion();
+};
+
+const renderQuestion = () => {
+  const questionElement = document.querySelector(".question");
+  const answeres = document.querySelector(".content");
+  const counter = document.querySelector(".count");
+  questionElement.innerHTML = `${user} ${questions[0].question}`;
+  answeres.innerHTML = questions[0].answers
+    .map((answer) => {
+      return `
+    <button onclick="select(this, '${answer.letter}')" id="${answer.letter}" class="answer">
+      <span class="letter">${answer.letter}</span> ${answer.answer}
+    </button>`;
+    })
+    .join("");
+  counter.textContent = `Question ${answered.length + 1}/${questions.length +
+    answered.length}`;
+};
+
+const select = (e, answer) => {
+  selected = answer;
+
+  if (selected) {
+    const previousElements = document.getElementsByClassName("active");
+    while (previousElements.length > 0) {
+      previousElements[0].classList.remove("active");
+    }
+  }
+  if (selected) {
+    document.querySelector(".next").classList.add("active");
+  }
+  document.getElementById(e.id).classList.add("active");
+  console.log(selected);
+};
+
+const setError = (text) => {
+  const errors = document.querySelector(".errorQuestion");
+  errors.innerText = text;
+  errors.style.color = "red";
+};
+
+const finishQuiz = () => {
+  const all = answered.length;
+  let correct = 0;
+  for (let i = 0; i < answered.length; i++) {
+    if (answered[i].selected === answered[i].correct) {
+      correct += 1;
+    }
+  }
+  const score = (correct / all) * 100;
+
+  const quizGame = document.querySelector(".game");
+
+  quizGame.innerHTML = `
+    <p class="count">Question ${answered.length}/${questions.length +
+    answered.length}</p>
+    <p class="question">${score}%</p>
+    <section class="content">
+      <p class="end">${user} vous avez fini avec ${correct} bonne réponses sur ${all}! voulez vous recommencer ?</p>
+    </section>
+    <button onclick="restart()" class="next">Recommencer</button>
+  `;
+  document.querySelector(".question").style.fontSize = "2.5rem";
+};
+
+const submitAnswer = () => {
+  document.querySelector(".next").classList.remove("active");
+  if (selected) {
+    const lastQuestion = questions.shift();
+    lastQuestion.selected = selected;
+    answered.push(lastQuestion);
+    selected = null;
+
+    if (questions.length) {
+      renderQuestion();
+    } else {
+      finishQuiz();
+    }
+  } else {
+    setError("You need to select answer.");
+  }
+};
+
+const generationQuiz = () => {
+  questions = [].concat(questionsTemplate);
+  answered = [];
+  selected = null;
+};
+const restart = () => {
+  generationQuiz();
+  renderTemplate();
+};
+
+generationQuiz();
